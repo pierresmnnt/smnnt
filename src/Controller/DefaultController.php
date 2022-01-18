3dc325d3e69a49f9ab766eca72d3b5ffdfb62d4e
Pierre Simonnet <pierresimonnet@protonmail.com>
2022-01-18 23:04:00 +0100

    misc

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\ArticleRepository;
-use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(ImageRepository $imageRepository, ArticleRepository $articleRepository): Response
+    public function index(): Response
     {
-        $lastImages = $imageRepository->findLast();
-        $lastArticles = $articleRepository->findLastPublished();
-
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-            'images' => $lastImages,
-            'articles' =>$lastArticles
-        ]);
+        return $this->render('default/index.html.twig');
     }
 
     #[Route('/ui', name: 'interface')]
