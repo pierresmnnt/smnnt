f9c1dcf2ffe7b30c607c035888411c1f12c0ef96
Pierre Simonnet <pierresimonnet@protonmail.com>
2022-09-21 23:12:42 +0200

    reset password

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
---
+++ src/Command/SendEmailCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+#[AsCommand(
+    name: 'app:send-email',
+    description: 'Add a short description for your command',
+)]
+class SendEmailCommand extends Command
+{
+    private SymfonyStyle $io;
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        parent::__construct();
+        $this->mailer = $mailer;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Send an email for testing')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $email = (new Email())
+            ->from('test@domain.com')
+            ->to('receiver@domain.com')
+            ->subject('This is a test email')
+            ->text('This is a test')
+        ;
+
+        $this->mailer->send($email);
+
+        $io->success('Test email sent ! Check your inbox.');
+
+        return Command::SUCCESS;
+    }
+}
