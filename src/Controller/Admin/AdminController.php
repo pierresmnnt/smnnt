2b84b77f548dc16ec3b8cb2b3a9383305b5b5d0a
Pierre Simonnet <pierresimonnet@protonmail.com>
2022-09-24 14:26:38 +0200

    user profile edit

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\BaseController;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin')]
-class AdminController extends AbstractController
+class AdminController extends BaseController
 {
     #[Route('/', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
+            'menu' => 'admin'
+        ]);
+    }
+
+    #[Route('/edit', name: 'admin_edit')]
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getEntityManager()->flush();
+
+            $this->addFlash('success', "User updated");
+
+            return $this->redirectToRoute('admin_edit', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/profile/edit.html.twig', [
+            'form' => $form,
             'menu' => 'admin'
         ]);
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
