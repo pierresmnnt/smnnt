1f225fc913691a15fa3bdd47e953d9a1567a9993
Pierre Simonnet <pierresimonnet@protonmail.com>
2023-08-05 18:28:48 +0200

    article ext links

--- assets/js/modules/externalLinks.js
+++ assets/js/modules/externalLinks.js
@@ -1,6 +1,7 @@
 const anchors = document.querySelector("article").getElementsByTagName("a");
 for (let i = 0; i < anchors.length; i++) {
   if (anchors[i].hostname != window.location.hostname) {
-    anchors[i].setAttribute("rel", "nofollow");
+    anchors[i].setAttribute("rel", "nofollow noreferrer noopener external");
+    anchors[i].setAttribute("target", "_blank");
   }
 }
